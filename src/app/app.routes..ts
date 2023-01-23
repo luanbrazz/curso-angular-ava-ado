7a1fcf933a6c7eb4d28e39759f0f850ebff29a57
Luan Braz <luan_silvacpv@hotmail.com>
2023-01-23 15:52:53 -0300

    Tratamento de erro 404 via url

--- src/app/app.routes..ts
+++ src/app/app.routes..ts
@@ -4,17 +4,22 @@ import { DataBindingComponent } from "./dem/data-binding/data-binding.component"
 import { CadastroComponent } from "./dem/reactiveForms/cadastro/cadastro.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 import { HomeComponent } from "./navegacao/home/home.component";
+import { NotFoundComponent } from "./navegacao/not-found/not-found.component";
 
 const rootRouterConfig: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    // {path: 'contato', component: ContatoComponent},
-    {path: 'sobre', component: SobreComponent},
-    {path: 'feature-data-binding', component: DataBindingComponent},
-    {path: 'cadastro', component: CadastroComponent},
-    {path: 'produtos',
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    { path: 'home', component: HomeComponent },
+    // {path: 'contato', component: ContatoComponent },
+    { path: 'sobre', component: SobreComponent },
+    { path: 'feature-data-binding', component: DataBindingComponent },
+    { path: 'cadastro', component: CadastroComponent} ,
+    { path: 'produtos',
             loadChildren: () => import('./dem/arquitetura-componentes/produto.module')
-            .then(m => m.ProdutoModule)}
+            .then(m => m.ProdutoModule) },
+
+    // erro 404 - **SEMPRE POR ULTIMO
+    { path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from "@angular/router";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
 import { MenuComponent } from "./menu/menu.component";
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
     FooterComponent,
     HomeComponent,
+    NotFoundComponent,
 
   ],
   imports: [
@@ -23,6 +25,7 @@ import { MenuComponent } from "./menu/menu.component";
     MenuComponent,
     FooterComponent,
     HomeComponent,
+    NotFoundComponent
   ]
 })
 export class NavegacaoModule {}
