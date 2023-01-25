30c10b8869868f507e752fbaf018470993cb5153
Luan Braz <luan_silvacpv@hotmail.com>
2023-01-25 08:57:04 -0300

    Obtendo parâmetros da rota

--- src/app/dem/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ src/app/dem/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from '../models/produto';
+import { ProdutoService } from '../services/produto.service';
 
 @Component({
   selector: 'app-editar-produto',
@@ -6,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarProdutoComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto;
+
+  constructor(
+    // pega a rota da rota ativa
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.produto = this.produtoService.obterPorId(params['id']);
+    })
+  }
+
+  salvar(){
+
   }
 
 }
--- src/app/dem/arquitetura-componentes/produto.module.ts
+++ src/app/dem/arquitetura-componentes/produto.module.ts
@@ -12,6 +12,7 @@ import { ProdutoDetalheComponent } from "./componentes/produto-card-detalhe.comp
 import { ProdutoCountComponent } from "./componentes/produto-count.component";
 import { EditarProdutoComponent } from './editar-produto/editar-produto.component';
 import { ProdutoAppComponent } from "./produto.app.component";
+import { ProdutoService } from "./services/produto.service";
 
 
 @NgModule({
@@ -26,6 +27,9 @@ import { ProdutoAppComponent } from "./produto.app.component";
     imports: [
         CommonModule,
         ProdutoRoutingModule,
+    ],
+    providers: [
+      ProdutoService,
     ]
 })
 export class ProdutoModule{
