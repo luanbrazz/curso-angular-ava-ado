78105700df09f25a088ef43738aa4b4a9cb72511
Luan Braz <luan_silvacpv@hotmail.com>
2023-01-26 21:33:20 -0300

    Providers UseValue - NgModules - Injection Token

--- src/app/dem/bar-di-zones/bar.module.ts
+++ src/app/dem/bar-di-zones/bar.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarDiZonesComponent } from './bar-di-zones.component';
+import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 
 @NgModule({
   declarations: [
@@ -13,4 +14,17 @@ import { BarDiZonesComponent } from './bar-di-zones.component';
     BarDiZonesComponent
   ]
 })
-export class BarDiZonesModule { }
+export class BarDiZonesModule {
+  static forRoot(arg0: { unidadeId: number; unidadeToken: string; }): any[] | import("@angular/core").Type<any> | ModuleWithProviders<{}> {
+    throw new Error('Method not implemented.');
+  }
+  // static forRoot(config: BarUnidadeConfig) : ModuleWithProviders {
+  //   return {
+  //     NgModule: BarDiZonesModule,
+  //     providers: [
+  //       { provide: 'ConfigManualUnidade', useValue: config },
+  //       { provide: BAR_UNIDADE_CONFIG, useValue: config }
+  //     ]
+  //   }
+  // }
+ }
